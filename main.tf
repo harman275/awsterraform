64cc670aa3d6ff98c810410ca589ab8881e24c55
harman275 <89581757+harman275@users.noreply.github.com>
2024-07-24 18:51:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Replace with your desired AMI ID
-  instance_type = "t2.micro"
+resource "aws_instance" "ec2" {
+  ami           = var.ami
+  instance_type = var.instance_type
 
   tags = {
-    Name = "example-instance"
+    Name = "test-instance"
   }
 }
